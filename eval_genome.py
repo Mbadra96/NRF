00f76e5c4866fb712e3eb2057d58f68df02ff1e7
Mohamed Badra <m.badra96@gmail.com>
2021-09-18 20:22:40 +0200

    Bi-Copter

--- eval_genome.py
+++ eval_genome.py
@@ -1,83 +1,80 @@
 from neuron.core.neuro_controller import NeuroController
 from neuron.simulation.levitating_ball import LevitatingBall
+from neuron.utils.randomizer import Randomizer
 from neuron.utils.units import *
 import numpy as np
 from neuron.optimizer.neat.genome import Genome
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
-from math import pi,sin
 
-TIME = 50 * sec
-TIMESTEP = 0.5 * ms # dt is 0.1 ms
-SAMPLES = int(TIME/TIMESTEP)
-t = np.arange(0,TIME,TIMESTEP)
+TIME = 10 * sec
+TIMESTEP = 0.5 * ms  # dt is 0.5 ms
+SAMPLES = int(TIME / TIMESTEP)
+t = np.arange(0, TIME, TIMESTEP)
+
 
 def clamp(e):
     if e > 0:
-        return 1,0
+        return 1, 0
     elif e < 0:
-        return 0,1
+        return 0, 1
     else:
-        return 0,0
-    
+        return 0, 0
 
 
-def eval_func(genome,show:bool=False)->float: 
+def eval_func(genome, show: bool = False, mass=1.0) -> float:
     input_signals = 1
     output_signals = 1
-    input_encoder_threshold = 0.00001
+    input_encoder_threshold = 0.0001
     output_decoder_threshold = 1
     output_base = 9.81
 
     cont = genome.build_phenotype(input_signals,output_signals,input_encoder_threshold,output_decoder_threshold,output_base,TIMESTEP)
-    # connection_matix = [[0.0,0.0,1.0,0.0],
-    #                     [0.0,0.0,0.0,1.0],
-    #                     [0.0,0.0,0.0,0.0],
-    #                     [0.0,0.0,0.0,0.0]]
-    # cont = NeuroController(input_signals,output_signals,input_encoder_threshold,output_decoder_threshold,output_base,connection_matix,[0,1],[2,3],TIMESTEP)
-    
+    # connection_matix = [[0.0, 0.0, 1.0, 0.0],
+    #                     [0.0, 0.0, 0.0, 1.0],
+    #                     [0.0, 0.0, 0.0, 0.0],
+    #                     [0.0, 0.0, 0.0, 0.0]]
+    # cont = NeuroController(input_signals, output_signals, input_encoder_threshold, output_decoder_threshold,
+    #                        output_base, connection_matix, [0, 1], [2, 3], TIMESTEP)
+
     if show:
-        v1 = [0]*SAMPLES
-        v2 = [0]*SAMPLES
-        v3 = [0]*SAMPLES
-        v4 = [0]*SAMPLES
+        v1 = [0] * SAMPLES
+        v2 = [0] * SAMPLES
+        v3 = [0] * SAMPLES
+        v4 = [0] * SAMPLES
 
-
-    
-    # x_ref = 2
+    x_ref = 2
     x_dot_ref = 0
     total_error = 0
     F = 0
-    x = 1
+    x = 0
     x_dot = 0
     e_I = 0
-    ball = LevitatingBall(1,x,x_dot)
+    ball = LevitatingBall(mass, x, x_dot)
 
     # Simulation Loop
     for i in range(SAMPLES):
-        x_ref = sin(pi*t[i]/10)+1
         e = (x_ref - x) + (x_dot_ref - x_dot)
-        e_I += 0.00001*e
-        total_error += abs((x_ref - x)/10.0)
+        e_I += 0.00001 * e
+        total_error += abs((x_ref - x) / 10.0)
         ###########################
-        # sensors = [*clamp(e)]
-        sensors = [e_I]
+        sensors = [*clamp(e)]
         ######################
-        output = cont.step(sensors,t[i],TIMESTEP) # Controller 
-        # F = 2*output[0] - 2*output[1] + 9.81 
+        output = cont.step(sensors, t[i], TIMESTEP)  # Controller
+        # F = 10*output[0] - 10*output[1] + 9.81
         F = output[0]
-        x, x_dot = ball.step(F,t[i],TIMESTEP) # Model
+        x, x_dot = ball.step(F, t[i], TIMESTEP)  # Model
         # print(f"in -> {sensors}, out -> {output}")
-        
+
         if show:
-            v1[i],v2[i]= x, x_dot
-            v3[i] = x_ref
-            v4[i] = e_I 
+            v1[i], v2[i] = x, x_dot
+            v3[i] = F
+            v4[i] = e_I
         else:
             pass
-    
+
     if show:
-        fig = make_subplots(rows=4, cols=1, subplot_titles=("x", "x_dot", "Force","e_I"))
+        fig = make_subplots(rows=4, cols=1, subplot_titles=("x", "x_dot", "Force", "e_I"))
 
         fig.add_trace(
             go.Scatter(x=t, y=v1),
@@ -91,7 +88,7 @@ def eval_func(genome,show:bool=False)->float:
 
         fig.add_trace(
             go.Scatter(x=t, y=v3),
-            row=1, col=1
+            row=3, col=1
         )
         fig.add_trace(
             go.Scatter(x=t, y=v4),
@@ -99,11 +96,78 @@ def eval_func(genome,show:bool=False)->float:
         )
         fig.update_layout(height=720, width=1080, title_text="Genome Test")
         fig.show()
-    
-    return total_error#+0.01*total_F
+
+    return total_error  # +0.01*total_F
+
+
+def _ball_levitation_eval_func_testing(genome, show: bool = False, mass: float = 1.0, disturbance: bool = False,
+                                       disturbance_magnitude=1.0, fig=None):
+    input_encoder_threshold = 0.0001
+    output_decoder_threshold = 1
+    output_base = 9.81
+
+    cont = genome.build_phenotype(1, 1, input_encoder_threshold, output_decoder_threshold,
+                                  output_base, TIMESTEP)
+
+    if show:
+        v1 = [0] * SAMPLES
+        v2 = [0] * SAMPLES
+        v3 = [0] * SAMPLES
+
+    x_ref = 2
+    x_dot_ref = 0
+    total_error = 0
+    x = 0
+    x_dot = 0
+    ball = LevitatingBall(mass, x, x_dot)
+
+    # Simulation Loop
+    for i in range(SAMPLES):
+        e = (x_ref - x) + (x_dot_ref - x_dot)
+        if disturbance:
+            e += Randomizer.Float(float(-disturbance_magnitude), float(disturbance_magnitude))
+        total_error += abs((x_ref - x) / 10.0)
+        ###########################
+        sensors = [*clamp(e)]
+        ######################
+        output = cont.step(sensors, t[i], TIMESTEP)  # Controller
+        f = output[0]
+        x, x_dot = ball.step(f, t[i], TIMESTEP)  # Model
+        # print(f"in -> {sensors}, out -> {output}")
+
+        if show:
+            v1[i], v2[i] = x, x_dot
+            v3[i] = f
+        else:
+            pass
+
+    if show:
+        if not fig:
+            fig = make_subplots(rows=3, cols=1, subplot_titles=("X", "X Dot", "Force"))
+
+        fig.add_trace(
+            go.Scatter(x=t, y=v1, name=f"X D={disturbance_magnitude:.2f}"),
+            row=1, col=1
+        )
+
+        fig.add_trace(
+            go.Scatter(x=t, y=v2, name=f"X Dot D={disturbance_magnitude:.2f}"),
+            row=2, col=1
+        )
+
+        fig.add_trace(
+            go.Scatter(x=t, y=v3, name=f"Force D={disturbance_magnitude:.2f}"),
+            row=3, col=1
+        )
+        fig.update_layout(height=720, width=1080, title_text="Disturbance Test")
+
+    return fig
+
 
 if __name__ == "__main__":
-    genome:Genome = Genome.load("best")
+    genome: Genome = Genome.load("best")
     # print(genome)
     # genome.visualize()
-    print(eval_func(genome,show=True))
+    f = _ball_levitation_eval_func_testing(genome, show=True, mass=1.0)
+    _ball_levitation_eval_func_testing(genome, show=True, mass=0.9, fig=f)
+    _ball_levitation_eval_func_testing(genome, show=True, mass=1.2, fig=f).show()
---
+++ neuron/simulation/bicopter.py
@@ -0,0 +1,99 @@
+from math import sin, cos, pi
+import numpy as np
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+
+
+class BiCopter:
+    def __init__(self, m=0.87, g=9.81, row=1.225,
+                 h=0.085, l=0.175, k_t=6.46,
+                 i_x=0.0043, i_y=0.0142, i_z=0.0176,
+                 i_xz=0.0001, x_dot_0=0, y_dot_0=0, z_dot_0=0,
+                 roll_dot_0=0, pitch_dot_0=0, yaw_dot_0=0,
+                 x_0=0, y_0=0, z_0=0,
+                 roll_0=0, pitch_0=0, yaw_0=0) -> None:
+        self.m = m
+        self.g = g
+        self.row = row
+        self.h = h
+        self.l = l
+        self.k_t = k_t
+        self.i_x = i_x
+        self.i_y = i_y
+        self.i_z = i_z
+        self.i_xz = i_xz
+        self.x_dot = x_dot_0
+        self.y_dot = y_dot_0
+        self.z_dot = z_dot_0
+        self.roll_dot = roll_dot_0
+        self.pitch_dot = pitch_dot_0
+        self.yaw_dot = yaw_dot_0
+        self.x = x_0
+        self.y = y_0
+        self.z = z_0
+        self.roll = roll_0
+        self.pitch = pitch_0
+        self.yaw = yaw_0
+
+    def step(self, w1, w2, t, dt):
+        u1 = self.k_t * ((w1 ** 2) + (w2 ** 2))
+        u2 = self.k_t * ((w1 ** 2) - (w2 ** 2))
+
+        self.x_dot += dt * ((-u1 / self.m) * (
+                    sin(self.roll) * sin(self.yaw) + cos(self.roll) * sin(self.pitch) * cos(self.yaw)))
+
+        self.y_dot += dt * ((u1 / self.m) * (
+                    sin(self.roll) * cos(self.yaw) - cos(self.roll) * sin(self.pitch) * sin(self.yaw)))
+
+        self.z_dot += dt*((-u1/self.m)*cos(self.roll)*cos(self.pitch) + self.g)
+
+        self.roll_dot += self.l*u2/self.i_x
+
+        self.x += dt * self.x_dot
+        self.y += dt * self.y_dot
+        self.z += dt * self.z_dot
+        self.roll += dt * self.roll_dot
+
+        return self.x_dot, self.y_dot, self.z_dot, self.roll_dot, self.x, self.y, self.z, self.roll
+
+    def evaluate_controller(self, controller) -> float:
+        pass
+
+
+if __name__ == '__main__':
+    TIME = 10
+    TIME_STEP = 0.5 * 0.001  # dt is 0.1 ms
+    SAMPLES = int(TIME / TIME_STEP)
+    t = np.arange(0, TIME, TIME_STEP)
+    bi_copter = BiCopter()
+    x = [0] * SAMPLES
+    y = [0] * SAMPLES
+    z = [0] * SAMPLES
+    roll = [0] * SAMPLES
+    w = 0.8127610321343152
+
+    for i in range(SAMPLES):
+        _, _, _, _, x[i], y[i], z[i], roll[i] = bi_copter.step(w+sin(t[i]*4*pi)*0.01, w+sin(t[i]*4*pi)*0.01, t[i], TIME_STEP)
+
+    fig = make_subplots(rows=4, cols=1)
+
+    fig.add_trace(
+        go.Scatter(x=t, y=x, name="x"),
+        row=1, col=1
+    )
+
+    fig.add_trace(
+        go.Scatter(x=t, y=y, name="y"),
+        row=2, col=1
+    )
+
+    fig.add_trace(
+        go.Scatter(x=t, y=z, name="z"),
+        row=3, col=1
+    )
+    fig.add_trace(
+        go.Scatter(x=t, y=roll, name="roll"),
+        row=4, col=1
+    )
+    fig.update_layout(height=720, width=1080, title_text="BI-COPTER TEST")
+    fig.show()
