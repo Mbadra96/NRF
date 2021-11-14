550b82fcb44faa15149c5fd8e294ebd5b3434d32
Mohamed Badra <m.badra96@gmail.com>
2021-11-14 19:22:50 +0200

    Updated Neuron SS

--- testing/neuron2.py
+++ testing/neuron2.py
@@ -5,43 +5,51 @@ import numpy as np
 
 class Neuron:
     def __init__(self):
-        self.v = 0
-        self.s = 0
-        self.R = 10
-        self.r = 0
+        self.v = np.zeros([2])
+        self.s = np.zeros([2])
+        # self.R = 10
+        self.r = np.zeros([2])
         self.dt = 0.0005
         self.tm = 0.001
+        self.Cv = exp(-self.dt/self.tm)
+        self.R = 1/(1-self.Cv)
+        self.Ci = self.R*(1-self.Cv)
+        self.weight_matrix = np.array([[0, 0], [1, 0]])
 
-    def step(self, i: float) -> None:
-        if self.r > 0:
-            self.r -= 1
-            self.v = 0
-            self.s = 0
-        else:
-            self.v = (exp(-self.dt/self.tm)*self.v) + ((self.R * i*self.dt)/self.tm)
-            if self.v >= 1.0:
-                # self.v = 0
-                self.s = 1
-                self.r = 4
+    def step(self, i_ext: list[float]) -> None:
+        # Minimum Current tm*(1-e^c)/(R dt) as
+        # it a lim when v = 1 => 1 = self.v = (self.c * 1) + ((self.R * i*self.dt)/self.tm)
+
+        i = np.array(i_ext) + self.weight_matrix @ self.s
+
+        self.v = ((self.Cv * self.v) + (self.Ci * i))*np.where(self.r == 0, 1, 0)
+
+        self.s = np.where(self.v >= 1, 1, 0)
+        self.r[self.r > 0] -= 1
+        self.r = np.where(self.v >= 1, 4, self.r)
 
 
 def main():
     t = np.arange(0, 0.1, 0.0005)
     n = Neuron()
 
-    v: list[list[float], list[float]] = [[], []]
+    v: list[list[float], list[float], list[float]] = [[], [], []]
 
     for i in range(len(t)):
-        n.step(1)
-        v[0].append(n.v)
-        v[1].append(n.s)
+        n.step([0.8, 0])
+        v[0].append(n.v[0])
+        v[1].append(n.s[0])
+        v[2].append(n.s[0]*t[i])
 
-    fig, ax = plt.subplots(2, 1, True)
+    fig, ax = plt.subplots(3, 1, sharex='all')
     ax[0].plot(t, v[0])
     ax[0].grid()
 
     ax[1].plot(t, v[1])
     ax[1].grid()
+
+    ax[2].eventplot(v[2], colors=(0, 1, 0))
+    ax[2].grid()
 
     plt.show()
 
